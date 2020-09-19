5401081281f1ab3b26af8e1f574e407cc08af48a
raquel <rhnoriega@outlook.com>
2020-09-19 14:06:48 -0500

    Updates

---
+++ precache-manifest.36adde6628e57c90ea73cfeda188c70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dca1a07639f105cfa5c7a4274f3fccd",
+    "url": "/fastfood-restaurant/index.html"
+  },
+  {
+    "revision": "cd89343e61960c80a37c",
+    "url": "/fastfood-restaurant/static/css/main.e63b0cdc.chunk.css"
+  },
+  {
+    "revision": "adc94e54dfef17fea256",
+    "url": "/fastfood-restaurant/static/js/2.1ba8df86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fastfood-restaurant/static/js/2.1ba8df86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd89343e61960c80a37c",
+    "url": "/fastfood-restaurant/static/js/main.6008291c.chunk.js"
+  },
+  {
+    "revision": "f20a6a308dc7cfa594c3",
+    "url": "/fastfood-restaurant/static/js/runtime-main.a34c6f89.js"
+  }
+]);
